be89c8a2c3b682e91b7d43654123eb4115b37bf8
Indiana Lora <indiana@ensaludybelleza.com>
2021-07-12 21:07:42 -0400

    created a todo list

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./components/Todo";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+
+function Todo() {
+  const [input, setInput] = useState("");
+  const [item, setItem] = useState([]);
+  const [edit, setEdit] = useState("");
+  const [show, setShow] = useState(false);
+
+  //Create
+  const handleSubmit = (event, input) => {
+    event.preventDefault();
+    if (input !== "") {
+      const idx = Math.floor(Math.random() * 100000);
+      setInput("");
+      setItem([...item, { id: idx, item: input }]);
+    }
+  };
+  //Update
+  const editItem = (itemId, newItem) => {
+    setShow(!show);
+    var editedList = item.filter(function (itemToEdit) {
+      return itemToEdit.id === itemId;
+    });
+    if (edit !== "" || null) {
+      editedList[0].item = newItem;
+      setEdit("");
+      setShow(false);
+    }
+  };
+  //Delete
+  const removeItem = (itemId) => {
+    var removeList = item.filter(function (itemToDelete) {
+      return itemToDelete.id !== itemId;
+    });
+    setItem(removeList);
+  };
+  //Check
+  const checkItem = (itemToCheck, event) => {
+    var checkItem = itemToCheck;
+    if (event === true) {
+      checkItem.classList = "check";
+    } else {
+      checkItem.classList.remove("check");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Write your productivity list! ☺</h1>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event, input);
+        }}
+      >
+        <input
+          type="text"
+          onChange={(event) => setInput(event.target.value)}
+          value={input}
+          placeholder="Write your todo!"
+        />
+        <button type="submit" value="submit">
+          Add item
+        </button>
+      </form>
+      {show ? (
+        <input
+          type="text"
+          onChange={(event) => setEdit(event.target.value)}
+          value={edit}
+          placeholder="Edit your todo!"
+        />
+      ) : null}
+      {item.map((items) => {
+        return (
+          <>
+            <h5 key={items.id}>
+              {items.item}
+              <input
+                type="checkBox"
+                onClick={(event) => {
+                  checkItem(event.target.parentElement, event.target.checked);
+                }}
+              />
+            </h5>
+
+            <button
+              onClick={() => {
+                editItem(items.id, edit);
+              }}
+            >
+              <i className="fas fa-edit"></i>
+            </button>
+            <button
+              onClick={() => {
+                removeItem(items.id);
+              }}
+            >
+              <i className="fas fa-trash-alt"></i>
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Todo;
